a48a4488027458d3eb0e8cee95fbe604d30a0d0c
Yunpopo7 <yunpopo179@gmail.com>
2026-04-13 05:05:42 +0900

    課題7章　追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,19 @@
+let num = 30;
+
+
+//変数numが3と5の倍数の時'3と5の倍数です'と出力する
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+//変数numが5の倍数の時'5の倍数です'と出力する
+else if (num % 5 === 0) {
+  console.log('5の倍数です');
+}
+//変数numが3の倍数の時'3の倍数です'と出力する
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+//変数numがそれ以外の時'変数numの値'を出力する
+else {
+  console.log(num);
+}
