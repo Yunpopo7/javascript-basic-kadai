a1c4437e80a5cddb20238b4ddc4e1f3ea9d8db72
Yunpopo7 <yunpopo179@gmail.com>
2026-04-25 22:29:18 +0900

    20章課題　再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,6 @@
 const text = document.getElementById('text');
+const btn = document.getElementById('btn');
 btn.addEventListener('click', () => {
-  text.textContent = 'ボタンがクリックされました'
+  text.textContent = 'ボタンをクリックしました'
 });
 
